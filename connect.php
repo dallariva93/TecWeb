29c200cdace438a3ff5c8e01509100816e15b4c1
andreazuddas <zuddas.andrea483@yahoo.it>
2016-11-08 16:03:37 +0100

    connessione

---
+++ connect.php
@@ -0,0 +1,24 @@
+<?php
+     $username = "andrea";
+     $password = "andrea";
+     $host = "localhost";
+     $database= "libri";
+     if (!( $db = new mysqli($host, $username, $password,$database))) 
+         {echo 'Errore durante la connessione al database';} 
+
+if ($mysqli->connect_errno) {
+    
+        echo "Il sito ha dei problemi di funzionamento.";
+
+  
+	    echo "Errore: Connessione fallita : \n";
+	        echo "Errno: " . $mysqli->connect_errno . "\n";
+		    echo "Error: " . $mysqli->connect_error . "\n";
+
+        exit;
+	}
+
+
+#      if (!(mysqli::select_db($database, $db)))
+#      {echo'Errore durante la selezione del database';}
+?>
