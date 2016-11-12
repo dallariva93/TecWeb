e4aa2e532b5b844c3619a86d3cbf191cddc6b107
andreazuddas <zuddas.andrea483@yahoo.it>
2016-11-12 14:09:12 +0100

    inserimento utente

--- inserisci_utente.php
+++ inserisci_utente.php
@@ -24,18 +24,18 @@ $nickname= $_POST['nickname'];
 $nome= $_POST['nome'];
 $cognome= $_POST['cognome'];
 $residenza= $_POST['residenza'];
-$datanascita= $_POST['datanascita'];
+$data= $_POST['data'];
 $password= $_POST['password'];
 $email= $_POST['email'];
 
-if (($nickname=="") or ($nome=="") or ($cognome=="") or ($residenza=="") or ($datanascita="") or ($password="") or ($email=="")) 
+if (($nickname=="") or ($nome=="") or ($cognome=="") or ($residenza=="") or ($data="") or ($password="") or ($email=="")) 
 { 
 echo "<br><h1>Errore, dati mancanti</h1>";
 } 
 else
 
 {
-$insert="INSERT INTO `utente` VALUES ('$email','$nome','$cognome','$nickname','$datanascita','$password', '$residenza')";
+$insert="INSERT INTO `utente` VALUES ('$email','$nome','$cognome','$nickname','$data','$password', '$residenza')";
 
 } 
 $result = mysqli_multi_query($db,$insert);
