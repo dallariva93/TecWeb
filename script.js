94bf402c86905dbfd58785a6b359b12be1535cc6
ldalmedi <ldalmedi@t25.torre.math.unipd.it>
2016-11-16 12:59:57 +0100

    Aggiunto mobile layout e varie modifiche minori

--- script.js
+++ script.js
@@ -1,17 +1,52 @@
 "use strict";
 var indice=0;
-slideShow(0);
-function cambioSlide(n) {
-    slideShow(indice += n);
+var tempo;
+slideShow();
+function slideShow() {
+	
+	var i;
+	var x = document.getElementsByClassName("slide");
+	var punti = document.getElementsByClassName("dot");
+	var lunghezza = x.length;
+	
+	if (indice >= lunghezza) {indice = 0;}
+	for (i = 0; i < lunghezza; i++) {
+	    x[i].style.display = "none";
+	}
+	for (i = 0; i < punti.length; i++) {
+	  punti[i].className = punti[i].className.replace(" active", "");
+	}
+	indice++;
+	x[indice-1].style.display = "block";
+	punti[indice-1].className += " active";
+	clearTimeout(tempo);    
+	tempo= setTimeout(slideShow, 2000);
 }
-function slideShow(n) {
-    var i;
+function mobileMenu() {
+    var x = document.getElementById("menu");
+    if (x.className === "menu") {
+        x.className += " mobile";
+    } else {
+        x.className = "menu";
+    }
+}
+
+function dotCambia(n) {
+	slideShow(indice=n);
+	 
+	 /*clearTimeout(tempo); ----> Cosi rimane fermo
+  	 var i;
     var x = document.getElementsByClassName("slide");
+    var punti = document.getElementsByClassName("dot");
     var lunghezza = x.length;
-    if (n > lunghezza) {indice = 1}
-    if (n < 1) {indice = lunghezza} ;
+    if (n >= lunghezza) {n = 0;}
     for (i = 0; i < lunghezza; i++) {
         x[i].style.display = "none";
     }
-    x[indice-1].style.display = "block";
+    for (i = 0; i < punti.length; i++) {
+      punti[i].className = punti[i].className.replace(" active", "");
+	 }
+	 n++;
+    x[n-1].style.display = "block";
+    punti[n-1].className += " active";*/
 }
