2073c6f7d0e9292ca2d1805be48ec291ac108ab7
andreazuddas <zuddas.andrea483@yahoo.it>
2016-11-29 14:25:08 +0100

    amministrazione

--- PHP&HTML/connect.php
+++ PHP&HTML/connect.php
@@ -7,9 +7,7 @@
          {echo 'Errore durante la connessione al database';} 
 
 if ($mysqli->connect_errno) {
-    
         echo "Il sito ha dei problemi di funzionamento.";
-
   
 	    echo "Errore: Connessione fallita : \n";
 	        echo "Errno: " . $mysqli->connect_errno . "\n";
@@ -17,7 +15,6 @@ if ($mysqli->connect_errno) {
 
         exit;
 	}
-
 
 #      if (!(mysqli::select_db($database, $db)))
 #      {echo'Errore durante la selezione del database';}
