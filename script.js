b6e6107c8efc82a91e5fd84a42b8efd2d4060d11
luca.dalmedico93@gmail.com <luca.dalmedico93@gmail.com>
2016-12-01 11:53:03 +0100

    Tolto slideshow

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
 "use strict";
-var indice=0;
-var tempo;
-slideShow();
-function slideShow() {
-	
-	var i;
-	var x = document.getElementsByClassName("slide");
-	var punti = document.getElementsByClassName("dot");
-	var lunghezza = x.length;
-	
-	if (indice >= lunghezza) {indice = 0;}
-	for (i = 0; i < lunghezza; i++) {
-	    x[i].style.display = "none";
-	}
-	for (i = 0; i < punti.length; i++) {
-	  punti[i].className = punti[i].className.replace(" active", "");
-	}
-	indice++;
-	x[indice-1].style.display = "block";
-	punti[indice-1].className += " active";
-	clearTimeout(tempo);    
-	tempo= setTimeout(slideShow, 2000);
-}
 function mobileMenu() {
     var x = document.getElementById("menu");
     if (x.className === "menu") {
@@ -29,24 +6,4 @@ function mobileMenu() {
     } else {
         x.className = "menu";
     }
-}
-
-function dotCambia(n) {
-	slideShow(indice=n);
-	 
-	 /*clearTimeout(tempo); ----> Cosi rimane fermo
-  	 var i;
-    var x = document.getElementsByClassName("slide");
-    var punti = document.getElementsByClassName("dot");
-    var lunghezza = x.length;
-    if (n >= lunghezza) {n = 0;}
-    for (i = 0; i < lunghezza; i++) {
-        x[i].style.display = "none";
-    }
-    for (i = 0; i < punti.length; i++) {
-      punti[i].className = punti[i].className.replace(" active", "");
-	 }
-	 n++;
-    x[n-1].style.display = "block";
-    punti[n-1].className += " active";*/
 }
