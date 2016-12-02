605ea87acbdffb74919b095fafe7f7cd51249512
luca.dalmedico93@gmail.com <luca.dalmedico93@gmail.com>
2016-12-02 18:49:50 +0100

    Fatto ordine e bug-fix

--- script.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function mobileMenu() {
-    var x = document.getElementById("menu");
-    if (x.className === "menu") {
-        x.className += " mobile";
-    } else {
-        x.className = "menu";
-    }
-}
