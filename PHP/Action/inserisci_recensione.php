acd6eedeac19f5986b2627cfc16bbd46711f15e1
Luca Dal Medico <luca.dalmedico93@gmail.com>
2017-01-04 16:22:29 +0100

    Spostate azione PHP nella cartella giusta

---
+++ PHP/Action/inserisci_recensione.php
@@ -0,0 +1,38 @@
+<?php
+
+include('connect.php');
+
+echo file_get_contents("../../HTML/Template/HeadAdmin.txt");
+
+echo file_get_contents("../../HTML/Template/MenuAdmin.txt");
+
+echo file_get_contents("../../HTML/Template/SearchAdmin.txt");
+
+$codice= $_POST['codice'];
+$libro= $_POST['libro'];
+$autore= $_POST['autore'];
+$testo= $_POST['testo'];
+$data= $_POST['data'];
+$valutazione= $_POST['valutazione'];
+
+if (($codice=="") or ($libro=="") or ($autore=="") or ($data=="") or ($testo=="") or ($valutazione=="")) 
+{ 
+echo "<br><h1>Errore, dati mancanti</h1>";
+} 
+else
+
+{
+$insert="INSERT INTO `Recensione` VALUES ('$codice','$libro','$autore','$data','$valutazione', '$testo')";
+} 
+
+$result = mysqli_multi_query($db,$insert);
+
+if($result){
+	echo("<br> <H1>Inserimento avvenuto correttamente</H1>");
+} else{
+	echo("<br><H1>Inserimento non eseguito</h1>");
+} 
+echo file_get_contents("../../HTML/Template/FooterAdmin.txt");
+?>
+
+
